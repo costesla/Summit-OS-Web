21e36f9981e3f82eb0713b9b96ae8081dfe6c9a7
Peter Teehan <peter.teehan@costesla.com>
2026-01-25 00:44:41 -0700

    Migration to Azure: Enable standalone output and fix SWA workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
+    output: 'standalone',
+    serverExternalPackages: ["@googlemaps/google-maps-services-js"],
 };
 
 module.exports = nextConfig;
