bdd133f442213a29dbdc851dcef46e5f25a8484d
Peter Teehan <peter.teehan@costesla.com>
2026-01-24 21:04:32 -0700

    Fix next.config.js: Bundle google-maps dependency (remove external config)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    serverExternalPackages: ["@googlemaps/google-maps-services-js"],
 };
 
 module.exports = nextConfig;
